1934f4c65e6cdeccb3154ccc2abf56efbaccb26c
TobyWinZ <39978173+tobyyu007@users.noreply.github.com>
2020-08-13 16:19:41 +0800

    完成範圍設定

--- QuickCrawProject/ViewController.swift
+++ QuickCrawProject/ViewController.swift
@@ -14,6 +14,11 @@ import Kanna
 class ViewController: NSViewController, WKNavigationDelegate {
 
     @IBOutlet weak var crawWebView: WKWebView!
+    @IBOutlet weak var dateFrom: NSDatePicker!
+    @IBOutlet weak var dateTo: NSDatePicker!
+    
+    var dateFromFormatted = ""
+    var dateToFormatted = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,10 +50,20 @@ class ViewController: NSViewController, WKNavigationDelegate {
                 }
             }
         }
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyy年M月dd日"
+        dateFormatter.locale = Locale(identifier: "zh_Hant_TW") // 設定地區(台灣)
+        dateFormatter.timeZone = TimeZone(identifier: "Asia/Taipei") // 設定時區(台灣)
+        dateFormatter.calendar = Calendar(identifier: Calendar.Identifier.republicOfChina)
+        dateFromFormatted = dateFormatter.string(from: dateFrom.dateValue)
+        dateToFormatted = dateFormatter.string(from: dateTo.dateValue)
     }
     
     func continousCraw() // 按下按鈕後繼續爬蟲動作
     {
+        searchDateRange.removeAll()
+        resultTime.removeAll()
+        resultDate.removeAll()
         let javascriptString = "document.querySelector('.cont90 .gap_t2 .std_btn').click();" // 按 "查詢場次" 按鈕
         crawWebView.evaluateJavaScript(javascriptString){ (value, error) in
             if let err = error {
@@ -71,48 +86,71 @@ class ViewController: NSViewController, WKNavigationDelegate {
     }
     
     var success = false // 已經有找到 available 的場次
-    static var resultDate = [String]() // 可以的場次日期
-    static var resultTime = [String]() // 可以的場次時間
+    var resultDate = [String]() // 可以的場次日期
+    var resultTime = [String]() // 可以的場次時間
+    var searchDateRange = [Int]()
     func parsehtml(_ html1: String)
     {
         let doc = try? Kanna.HTML(html: html1, encoding:.utf8)
-        for i in 5...19 // 只抓 17 號到 20 號
+        for i in 1...81 // 搜尋包含所要日期的 index
         {
-            for number in doc!.xpath("//*[@id='trnTable']/tbody/tr[\(i)]/td[3]")
+            for date in doc!.xpath("//*[@id='trnTable']/tbody/tr[\(i)]/td[1]")
             {
-                if number.text!.trimmingCharacters(in: .whitespacesAndNewlines) != "額滿"
+                if date.text!.trimmingCharacters(in: .whitespacesAndNewlines).contains(dateFromFormatted)
                 {
-                    for date in doc!.xpath("//*[@id='trnTable']/tbody/tr[\(i)]/td[1]") // 日期
-                    {
-                        ViewController.resultDate.append(date.text!.trimmingCharacters(in: .whitespacesAndNewlines))
-                    }
-                    for date in doc!.xpath("//*[@id='trnTable']/tbody/tr[\(i)]/td[2]") // 時間
-                    {
-                        let time = date.text!.trimmingCharacters(in: .whitespacesAndNewlines)[26...] // 把前面不需要的訊息去除
-                        ViewController.resultTime.append(time)
-                    }
-                    success = true // 有找到場次
-                    break
+                    searchDateRange.append(i)
+                }
+                else if date.text!.trimmingCharacters(in: .whitespacesAndNewlines).contains(dateToFormatted)
+                {
+                    searchDateRange.append(i)
                 }
             }
         }
-        if !success // 沒有找到
+        
+        if !searchDateRange.isEmpty
+        {
+            for i in searchDateRange[0]...searchDateRange[searchDateRange.count-1] // 只抓 17 號到 20 號
+            {
+                for number in doc!.xpath("//*[@id='trnTable']/tbody/tr[\(i)]/td[3]")
+                {
+                    if number.text!.trimmingCharacters(in: .whitespacesAndNewlines) != "額滿"
+                    {
+                        for date in doc!.xpath("//*[@id='trnTable']/tbody/tr[\(i)]/td[1]") // 日期
+                        {
+                            resultDate.append(date.text!.trimmingCharacters(in: .whitespacesAndNewlines))
+                        }
+                        for date in doc!.xpath("//*[@id='trnTable']/tbody/tr[\(i)]/td[2]") // 時間
+                        {
+                            let time = date.text!.trimmingCharacters(in: .whitespacesAndNewlines)[26...] // 把前面不需要的訊息去除
+                            resultTime.append(time)
+                        }
+                        success = true // 有找到場次
+                        break
+                    }
+                }
+            }
+            if !success // 沒有找到
+            {
+                continousCraw()
+            }
+            else // 有找到
+            {
+                success = false
+                showNotification() // 顯示場次的 notification
+                continousCraw()
+            }
+        }
+        else
         {
             continousCraw()
-        }
-        else // 有找到
-        {
-            //performSegue(withIdentifier: "success", sender: self)
-            success = false
-            showNotification() // 顯示場次的 notification
         }
     }
     
     func showNotification() -> Void // notificaiton 設定
     {
         let notification = NSUserNotification()
-        notification.title = ViewController.resultDate[0]
-        notification.subtitle = ViewController.resultTime[0]
+        notification.title = resultDate[0]
+        notification.subtitle = resultTime[0]
         notification.soundName = NSUserNotificationDefaultSoundName
         notification.deliveryDate = Date(timeIntervalSinceNow: 0)
         NSUserNotificationCenter.default.scheduleNotification(notification)
